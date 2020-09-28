99204f46a9a8574928898380dd0e282d6b8769a8
Marina8989 <marina.mill209@gmail.com>
2020-09-28 13:22:39 -0700

    flask practice

--- further-study/app.py
+++ further-study/app.py
@@ -13,3 +13,22 @@ def ask_story():
     return render_template("select-story.html", stories=stories.values())
 
 
+@app.route("/questions")
+def ask_questions():
+    """Generate and show form to ask words"""
+    story_id = request.args["story_id"]
+    story = stories[story_id]
+
+    prompts = story.prompts
+    return render_template("questions.html", story_id=story_id, title=story.title, prompts=prompts)
+
+
+@app.route("/story")
+def show_story():
+    """Show story result"""
+
+    story_id = request.args["story-id"]
+    story = stories[story_id]
+
+    text = story.generate(request.args)
+    return render_template("story.html", title=story.title, text=text)    
