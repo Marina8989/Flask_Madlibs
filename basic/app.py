9af4c73f5d6337cb7b3689ca1365261fad314b16
Marina8989 <marina.mill209@gmail.com>
2020-09-28 12:32:00 -0700

    flask practice

--- basic/app.py
+++ basic/app.py
@@ -6,3 +6,15 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
 debug = DebugToolbarExtension(app)
 
+@app.route("/")
+def ask_questions():
+    """Generate and show form to ask words"""
+    prompts = story.prompts
+    return render_template("questions.html", prompts=prompts)
+
+
+@app.route("/story")
+def show_story():
+    """Show story result"""
+    text = story.generate(request.args)
+    return render_template("story.html", text=text)
